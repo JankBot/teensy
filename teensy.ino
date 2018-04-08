c5a8010e6324c7e0e30e16df36da72469e9a8d8a
dylanhart <dylan96hart@gmail.com>
2018-04-08 11:50:33 -0700

    add more named constants and a watchdog to kill the bot on no comms

--- teensy.ino
+++ teensy.ino
@@ -1,47 +1,67 @@
 #include <Servo.h>
 #include <Wire.h>
+
 #define PHOTO_THRESHOLD 200
+#define WATCHDOG_TIMEOUT 20
+
+const int photo = A0;
+const int start_btn = 23;
+const int status_led = 13;
+
 Servo servo;
 Servo esc;
-const int photo = A0;
+
 byte bot_speed = 90, dir = 90;
+byte watchdog_count = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  // set pin modes
+  pinMode(status_led, OUTPUT);
+  pinMode(start_btn, INPUT_PULLUP);
+  pinMode(photo, INPUT_PULLUP);
+
+  // setup servo channels
   servo.attach(6); // steering
   esc.attach(5); // speed
-  pinMode(13, OUTPUT);
-  pinMode(23, INPUT_PULLUP);
-  pinMode(photo, INPUT_PULLUP);
-  arm();
+
+  // start inbound I2C
   Wire.begin(19);
   Wire.onReceive(controlEvent);
+
+  // Wait for esc to arm
+  arm();
 }
 
 void arm() {
   esc.write(90);
-  digitalWrite(13, HIGH);
-  while(digitalRead(23)){
+
+  // wait for manual signal to proceeed
+  digitalWrite(status_led, HIGH);
+  while(digitalRead(start_btn)){
     delay(1);  
   }
-  digitalWrite(13, LOW);
+  digitalWrite(status_led, LOW);
 }
 
 void controlEvent(int n) {
-  while (Wire.available() < 2);
+  if (n < 2) return;
+
   bot_speed = Wire.read();
   dir = Wire.read();
+  watchdog_count = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-   servo.write(dir);
-   esc.write(bot_speed);
-  
-//  servo.write(110);
-////  esc.write(100);
+  if (watchdog_count > WATCHDOG_TIMEOUT) {
+    dir = 0;
+    bot_speed = 0;
+  }
+
+  servo.write(dir);
+  esc.write(bot_speed);
+  watchdog_count++;
+
   delay(10);
-//  Serial.println(analogRead(photo));
 }
 
 //This code can be used for any purpose.
